20c14171c5b842c73720d005443318c42d85bb95
isabelli c pereira <isabellipereira58@gmail.com>
2024-02-18 23:46:54 -0300

    trabalhando com tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import Home from './paginas/home/home';
-import './App.css';
 import React from 'react';
+import './App.css';
+import Home from './paginas/home/home';
 
 function App() {
- 
   return (
     <>
-      <Home/>
+    <Home/>
     </>
-  );
+);
 }
-
 export default App;
